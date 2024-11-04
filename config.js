a4e21150ac6cd60d8bd3e4d96b204b6905d2d7e7
Charuka1 <129839125+Charuka1@users.noreply.github.com>
2024-11-04 15:31:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 
 
 module.exports = {
-SESSION_ID : process.env.SESSION_ID === undefined ? '' : process.env.SESSION_ID ,
+SESSION_ID : process.env.SESSION_ID === undefined ? 'cEQXyIDK#khG8zfTJaBKWm7YDwuoHNk3h96i3XVhFLtuBzWhNff8' : process.env.SESSION_ID ,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? "" : process.env.GITHUB_AUTH_TOKEN,
 GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? "" : process.env.GITHUB_USERNAME,
 BOT_NUMBER: process.env.BOT_NUMBER === undefined ? "" : process.env.BOT_NUMBER,
